5763c3875fbd9c36d823ce5af5ef70578df33af1
Jackson Godwin <jaxjunkmail80@gmail.com>
2020-10-14 18:22:58 -0400

    final

--- app.js
+++ app.js
@@ -1,84 +1,245 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./models');
+const es6Renderer = require('express-es6-template-engine');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const store = new SequelizeStore({ db: db.sequelize })
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret', // used to sign the cookie
+    resave: false, // update session even w/ no changes
+    saveUninitialized: true, // always create a session
+    store: store,
+}))
+
+store.sync();
+
 
 app.use(express.static('./public'));
 
-let todoList = [
-  {
-    id: 1,
-    todo: 'Implement a REST API',
-  },
-];
+app.engine('html', es6Renderer); // use es6renderer for html view templates
+app.set('views', 'templates'); // look in the 'templates' folder for view templates
+app.set('view engine', 'html'); // set the view engine to use the 'html' views
+
+function checkAuth(req, res, next){
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+app.get('/', checkAuth, (req, res) => {
+  res.render('index', {
+    locals: {
+      user: req.session.user
+    }
+  });
+})
+
+app.get('/register', (req, res) => {
+  res.render('register', {
+    locals: {
+      error: null,
+    }
+  });
+})
+
+app.post('/register', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.render('register', {
+      locals: {
+        error: 'Please submit all required fields'
+      }
+    })
+    return;
+  }
+  const { email, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    db.User.create({
+      email: email,
+      password: hash
+    })
+      .then((user) => {
+        res.redirect('/login');
+      }) 
+  })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', {
+    locals: {
+      error: null,
+    }
+  });
+})
+
+app.post('/login', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.render('login', {
+      locals: {
+        error: 'Please submit all required fields'
+      }
+    })
+    return;
+  }
+
+  db.User.findOne({
+    where: {
+      email: req.body.email
+    } 
+  })
+    .then((user) => {
+      if (!user) {
+        res.render('login', {
+          locals: {
+            error: 'No user with that email'
+          }
+        })
+        return;
+      }
+      
+      bcrypt.compare(req.body.password, user.password, (err, matched) => {
+        if (matched) {
+          req.session.user = user;
+          res.redirect('/');
+        } else {
+          res.render('login', {
+            locals: {
+              error: 'Incorrect Password: Please try again'
+            }
+          })
+        }
+        return;
+      })
+    })
+})
+
+app.get('/logout', (req, res) => {
+  req.session.user = null;
+  res.redirect('/login');
+})
+
+app.use('/api*', checkAuth);
 
 // GET /api/todos
 app.get('/api/todos', (req, res) => {
-  res.json(todoList);
+  db.Todo.findAll({
+    where: {
+      UserId: req.session.user.id
+    }
+  })
+    .then((todos) => {
+      res.json(todos);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({error: 'A database error occurred'});
+    })
 });
 
 // GET /api/todos/:id
 app.get('/api/todos/:id', (req, res) => {
-  const todo =
-    todoList.find((todo) => {
-      return todo.id === Number.parseInt(req.params.id);
-    }) || {};
-  const status = Object.keys(todo).length ? 200 : 404;
-  res.status(status).json(todo);
+  const { id } = req.params;
+  db.Todo.findOne({
+    where: {
+      id: id,
+      UserId: req.session.user.id
+    }
+  })
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+        return;
+      }
+      res.json(todo);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({error: 'A database error occurred'});
+    })
 });
 
 // POST /api/todos
 app.post('/api/todos', (req, res) => {
-  if (!req.body || !req.body.todo) {
+  if (!req.body || !req.body.name) {
     res.status(400).json({
       error: 'Provide todo text',
     });
     return;
   }
-  const prevId = todoList.reduce((prev, curr) => {
-    return prev > curr.id ? prev : curr.id;
-  }, 0);
-  const newTodo = {
-    id: prevId + 1,
-    todo: req.body.todo,
-  };
-  todoList.push(newTodo);
-  res.json(newTodo);
+
+  db.Todo.create({
+    name: req.body.name,
+    UserId: req.session.user.id
+  })
+    .then((newTodo) => {
+      res.json(newTodo);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({error: 'A database error occurred'});
+    })
+
 });
 
 // PUT /api/todos/:id
 app.put('/api/todos/:id', (req, res) => {
-  if (!req.body || !req.body.todo) {
+  if (!req.body || !req.body.name) {
     res.status(400).json({
       error: 'Provide todo text',
     });
     return;
   }
-  let updatedTodo = {};
-  todoList.forEach((todo) => {
-    if (todo.id === Number.parseInt(req.params.id)) {
-      todo.todo = req.body.todo;
-      updatedTodo = todo;
+  // const { id } = req.params;
+  db.Todo.findOne( {
+    where: {
+      id: id,
+      UserId: req.session.user.id
     }
-  });
-  const status = Object.keys(updatedTodo).length ? 200 : 404;
-  res.status(status).json(updatedTodo);
+  })
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+        return;
+      }
+      todo.name = req.body.name;
+      todo.save()
+      res.json(todo);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({error: 'A database error occurred'});
+    })
 });
 
 // DELETE /api/todos/:id
 app.delete('/api/todos/:id', (req, res) => {
-  let found = false;
-  todoList = todoList.filter((todo) => {
-    if (todo.id === Number.parseInt(req.params.id)) {
-      found = true;
-      return false;
+  // const { id } = req.params;
+  db.Todo.destroy({
+    where: {
+      id: id,
+      UserId: req.session.user.id
     }
-    return true;
-  });
-  const status = found ? 200 : 404;
-  res.status(status).json(todoList);
+  })
+    .then((deleted) => {
+      if (deleted === 0) {
+        res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+        return
+      }
+      res.status(204).json()
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({error: 'A database error occurred'});
+    })
 });
 
 app.listen(3000, function () {
